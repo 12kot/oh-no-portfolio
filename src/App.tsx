695726c48133a21f2b3d943b14e22b0ffb2eadc7
Nikita Yakoltsevich <Art.artem.nik@gmail.com>
2023-08-02 21:30:27 +0300

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from "./App.module.scss"
 import Banner from './components/banner/banner';
+import Header from './components/header/header';
 
 const App = () => {
   return (
-    <div className={styles.container}>
-      <Banner />
+    <div className={`${styles.container}`}>
+      <Header />
+      {/* <Banner /> */}
     </div>
   );
 }
---
+++ src/components/header/header.tsx
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from "react";
+import styles from "./header.module.scss";
+import dino from "../../img/dino.webp";
+
+const Header = () => {
+  const [bgActive, setBgActive] = useState<boolean>(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", function () {
+      if (window.scrollY >= 65) setBgActive(true);
+      else setBgActive(false);
+    });
+  }, []);
+
+  return (
+    <header className={`${styles.container} ${bgActive && styles.active}`}>
+      <img className={styles.dino} src={dino} alt="LOGO" height={"70%"}></img>
+
+      <section className={styles.menu}>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <button className={styles.lng}>lng</button>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
