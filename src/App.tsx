0d9476b1da6ecc5f129aa3a5d8216396743e3911
Nikita Yakoltsevich <Art.artem.nik@gmail.com>
2023-08-02 23:13:08 +0300

    edited colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import styles from "./App.module.scss"
-import Banner from './components/banner/banner';
-import Header from './components/header/header';
+import React from "react";
+import styles from "./App.module.scss";
+import Banner from "./components/banner/banner";
+import Header from "./components/header/header";
 
 const App = () => {
   return (
-    <div className={`${styles.container}`}>
+    <>
       <Header />
-      {/* <Banner /> */}
-    </div>
+      <div className={`${styles.container}`}>
+        {/* <Banner /> */}
+        <main className={styles.main}>
+          <p>WHO'S</p>
+          <p className={styles.nikita}>NIKITOSHA ?</p>
+        </main>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
